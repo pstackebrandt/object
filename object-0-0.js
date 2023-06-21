afaa91760186b67b29c9d6c19c32c52a8620dfbc
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-06-21 14:43:29 +0200

    first commit

---
+++ object-0-0.js
@@ -0,0 +1,10 @@
+const ullaHome = {
+    strasse: 'Hollstrasse',
+    hausnummer: 7,
+    postleitzahl: 18273,
+    ort: 'Güstrow',
+    eigentuemer: "Herr Müller"
+}
+
+console.log(ullaHome);
+// node .\object-0-0.js

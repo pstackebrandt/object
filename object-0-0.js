b534ff07043f612029cd237d8d5e6a996ac68390
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-06-21 14:51:27 +0200

    Add toString() for address (lesson 0-1)

--- object-0-0.js
+++ object-0-0.js
@@ -3,8 +3,29 @@ const ullaHome = {
     hausnummer: 7,
     postleitzahl: 18273,
     ort: 'Güstrow',
-    eigentuemer: "Herr Müller"
+    eigentuemer: "Herr Müller",
+    toString: function() {
+        return this.strasse + ' ' + this.hausnummer + ', ' + this.postleitzahl + ' ' + this.ort + ', ' + this.eigentuemer;
+    }
 }
 
 console.log(ullaHome);
+console.log(ullaHome.toString());
 // node .\object-0-0.js
+
+/* 
+Objekt 0.0
+
+Lege ein Objekt an, das für ein Gebäude die folgenden Informationen enthält:
+
+Strasse
+Hausnummer
+Postleitzahl
+Ort
+Eigentümer 
+
+Objekt 0.1
+Füge zu dem Objekt eine Methode hinzu, welche als Rückgabewert die gesamte Adresse als String liefert.
+Benutze diese Methode, um die gesamte Adresse auszugeben.
+*/
+
